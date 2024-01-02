e1760f6906f8e779577443cdceef077184b580e7
SolineMP <56126066+SolineMP@users.noreply.github.com>
2024-01-02 12:19:11 +0100

    add cart test

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,64 +1,80 @@
 import { faker } from '@faker-js/faker'
 const negativeNumber = faker.datatype.number({ min: -10, max: -1 }).toString();
+const apiUrl = Cypress.env("apiUrl")
 describe('Cart tests', () => {
     beforeEach(() => {
         cy.Connect()
         cy.url().should('include', '/');
     })
-    // it('should add a product with valid stock in the cart and update stock', () => {
-    //     cy.getBySel('product-home-link').first().click();
+    it('should add a product with valid stock in the cart and update stock', () => {
+        cy.getBySel('product-home-link').first().click();
       
-    //     cy.url().then((productPage) => {
-    //       cy.log(productPage);  
-    //       cy.getBySel('detail-product-stock')
-    //         .invoke('text')
-    //         .should('match', /^(0|[1-9][0-9]*) en stock$/)
-    //         .then((text) => {
-    //           const stockText = text.trim();
-    //           const stockNumber = parseInt(stockText.match(/\d+/));
-    //           cy.log(`Stock initial: ${stockNumber}`);
+        cy.url().then((productPage) => {
+          cy.log(productPage);  
+          cy.getBySel('detail-product-stock')
+            .invoke('text')
+            .should('match', /^(0|[1-9][0-9]*) en stock$/)
+            .then((text) => {
+              const stockText = text.trim();
+              const stockNumber = parseInt(stockText.match(/\d+/));
+              cy.log(`Stock initial: ${stockNumber}`);
       
-    //           cy.getBySel('detail-product-add').click(); 
-    //           cy.visit(productPage); 
-    //           cy.reload();      
-    //           const newStock = stockNumber - 1;
-    //           cy.getBySel('detail-product-stock')
-    //             .invoke('text')
-    //             .should('match', new RegExp(`^${newStock} en stock$`));
-    //         });
-    //     });
-    //   });
+              cy.getBySel('detail-product-add').click(); 
+              cy.visit(productPage); 
+              cy.reload();      
+              const newStock = stockNumber - 1;
+              cy.getBySel('detail-product-stock')
+                .invoke('text')
+                .should('match', new RegExp(`^${newStock} en stock$`));
+            });
+        });
+        cy.visit('/cart')
+        cy.getBySel('cart-line-delete').click({ multiple : true })
+      });
 
-    //   it('shouldn\'t change the cart with negative number ', () => {
-    //     cy.getBySel('product-home-link').first().click()
-    //     cy.getBySel('detail-product-quantity').clear().type(negativeNumber)
-    //     cy.getBySel('detail-product-add').click()
-    //     cy.getBySel('detail-product-form').should('have.class', 'ng-invalid')    
-    //   })
+    it('shouldn\'t change the cart with negative number', () => {
+      cy.getBySel('product-home-link').first().click();
+      cy.getBySel('detail-product-quantity').clear().type(negativeNumber);
+      cy.getBySel('detail-product-add').click();
+      cy.getBySel('detail-product-form').should('have.class', 'ng-invalid');
+    });
+    
+    it('should adds a product to the cart and shows up in the API', () => {
+      let id;
+      cy.getBySel('product-home-link').first().click()
+      
+      cy.url().then(url => {
+        const segments = url.split('/') 
+        id = parseInt(segments[segments.length - 1]);
+        cy.log(id)
+    
+        cy.wait(2000);
 
-    //   it('shouldn\'t change the cart with 20+ number', () => {
-    //     cy.getBySel('product-home-link').first().click()
-    //     cy.getBySel('detail-product-quantity').clear().type(21)
-    //     cy.getBySel('detail-product-add').click()
-    //     cy.getBySel('detail-product-form').should('have.class', 'ng-invalid')  
-    //   })
-
-      it('should add a product to the cart and show up in the API', () => {
-        cy.getBySel('product-home-link').first().click()
-        cy.url().then(url => {
-            const id = new URL(url).searchParams.get('id')
-            cy.log(id)
-
-        })
-
-        // cy.getBySel('detail-product-add').click()
-
-
+        cy.contains('[data-cy="detail-product-add"]', 'Ajouter au panier').click();
+    
+        cy.getToken().then((token) => {
+          cy.request({
+            method: 'GET',
+            url: apiUrl + '/orders',
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }).then((response) => {
+            cy.log(response.body.orderLines[0].product.id);
+            const idInApi = response.body.orderLines[0].product.id
+            expect(idInApi).to.equal(id);
+          });
+        });
       })
-
-    // after(() => {
-    //     cy.visit('/cart')
-    //     cy.getBySel('cart-line-delete').click({ multiple : true })
-    // })  
+      cy.visit('/cart')
+      cy.getBySel('cart-line-delete').click({ multiple : true })
+    });
+     
+    it('shouldn\'t change the cart with 20+ number', () => {
+      cy.getBySel('product-home-link').first().click()
+      cy.getBySel('detail-product-quantity').clear().type(21)
+      cy.getBySel('detail-product-add').click()
+      cy.getBySel('detail-product-form').should('have.class', 'ng-invalid')
     })
+  })
          
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
+const apiUrl = Cypress.env("apiUrl")
 Cypress.Commands.add("getBySel", (selector, ...args) => {
     return cy.get(`[data-cy=${selector}]`, ...args)
   })
@@ -35,4 +10,19 @@ Cypress.Commands.add("getBySel", (selector, ...args) => {
     cy.getBySel('login-input-username').type(username);
     cy.getBySel('login-input-password').type(password);
     cy.getBySel('login-submit').click();
+  });
+
+
+
+  Cypress.Commands.add('getToken', () => {
+    return cy.request({
+      method: 'POST',
+      url: apiUrl + '/login',
+      body: {
+        username: Cypress.env('username'),
+        password: Cypress.env('password'),
+      },
+    }).then((response) => {
+      return response.body.token;
+    });
   });
